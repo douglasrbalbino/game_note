fe61f6657cd78625da94e70384c08bf773c31fa0
Douglas <doug.balbino@hotmail.com>
2025-11-27 19:51:52 -0300

    primeiro commit do projeto com a organização básica

---
+++ android/app/src/main/kotlin/com/example/game_note_application/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.game_note_application
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:game_note_application/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+
